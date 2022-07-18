ca4a554e111fae70e8c22364e0aac2a0230f56ae
BlackWizlock <aa.salikov@mail.ru>
2022-07-18 19:05:53 +0300

    init

--- app/api/movies/movies.py
+++ app/api/movies/movies.py
@@ -5,8 +5,8 @@ movies_ns = Namespace('movies')
 
 @movies_ns.route('/')
 class MoviesView(Resource):
-    def read(self):
+    def get(self):
         return "", 200
 
-    def read_id(self, uid):
+    def get_id(self, uid):
         return "", 200
